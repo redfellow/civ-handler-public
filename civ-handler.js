a8f4c9b0cd13ba07504a13a2449b524c14f27e93
reiska <reijo.nyberg+github@gmail.com>
2020-11-05 00:20:51 +0200

    [MOD] logging events and errors to JSON files, log.html page for viewing logs, various updates to logic

--- civ-handler.js
+++ civ-handler.js
@@ -1,3 +1,5 @@
+const moment = require("moment");
+
 module.exports = {
 
 	startApp: function (app) {
@@ -6,26 +8,77 @@ module.exports = {
 
 		setInterval(function () {
 			console.log(new Date(), `[✔] civ-handler >> root > still listening for POSTs..`)
-		}, 1500000)
+		}, APP_ALIVE_INTERVAL)
 	},
 
+	handleAlive: function (req, res) {
+		console.log(new Date(), `[✔] civ-handler >> app.get > Alive request received`)
+		res.status(200).send({
+			timestamp: new Date(),
+			message: `[⚠] civ-handler >> handleAlive > Alive request handled`			
+		})
+	},
+
+	showLog: function (req, res) {
+		const fs = require('fs')
+		const forwarded = req.headers['x-forwarded-for']
+		const ip = forwarded ? forwarded.split(/, /)[0] : req.connection.remoteAddress
+	
+		console.log(new Date(), `[✔] civ-handler >> app.get > Log request received from ${ip}`)
+	
+		res.render(__dirname + "/log.html", { 
+			moment: moment,
+			SENT_MESSAGES : SENT_MESSAGES,
+			ERRORS : ERRORS
+		});
+
+		fs.writeFile('lastshow.log', ip, (err) => {
+			if (err) throw err;
+		});
+	},
 
 	handlePost: function (req, res) {
+		const fs = require('fs')
+		const forwarded = req.headers['x-forwarded-for']
+		const ip = forwarded ? forwarded.split(/, /)[0] : req.connection.remoteAddress
 		console.log(new Date(), `[✔] civ-handler >> app.post > POST received`, req.body)
-		CIV.setVars(req.body);
+
+		CIV.setVars(req.body, ip);
 		CIV.setHook()
 		CIV.duplicateCheck()
 
 		if ("errorMsg" in VARS === false) CIV.sendDiscordMessage(req, res)
-		else CIV.handleError(req, res)
+		else CIV.handleError(req, res, ip)		
+		
+		fs.writeFile('log.json', JSON.stringify(SENT_MESSAGES), (err) => {
+			if (err) throw err;
+		});
+		
+		fs.writeFile('errors.json', JSON.stringify(ERRORS), (err) => {
+			if (err) throw err;
+		});
+
+		var lastpostData = JSON.stringify({
+			ip: ip,
+			post: req.body
+		});
+
+		fs.writeFile('lastpost.log', lastpostData, (err) => {
+			if (err) throw err;
+		});
 	},
 
 
-	setVars: function (json) {
+	setVars: function (json, ip) {
+		var date = new Date();
 		VARS = {
+			time: moment().format("DD.MM.YYYY - H:mm:ss"),
+			epoch: moment().valueOf(),
 			game: json.value1,
 			player: USER_MAP[json.value2] || json.value2,
+			playerRaw: json.value2,
 			turn: json.value3,
+			ip: ip
 		}
 
 		VARS.msgTitle = `Hey ${VARS.player}, it's your turn to play!`
@@ -41,7 +94,7 @@ module.exports = {
 				return
 			}
 		}
-
+		
 		VARS.errorMsg = `'${VARS.game}' could not be matched to any of the defined HOOKS`
 	},
 
@@ -66,30 +119,30 @@ module.exports = {
 			message: `[✔] civ-handler >> handlePost > request passed to discord successfully`,
 			VARS: VARS,
 			POSTRequestHeaders: req.headers,
-			remoteAddress: req.connection.remoteAddress
 		})
 	},
 
 
-	handleError: function (req, res) {
+	handleError: function (req, res, ip) {
 		console.warn(new Date(), `[⚠] civ-handler >> handleError > ${VARS.errorMsg}`)
 		if (typeof VARS.hook === "object") delete VARS.hook
-		res.status(500).send({
+		ERRORS[new Date().valueOf()] = VARS;
+		res.status(400).send({
 			timestamp: new Date(),
 			message: `[⚠] civ-handler >> handleError > ${VARS.errorMsg}`,
 			VARS: VARS,
 			POSTRequestHeaders: req.headers,
-			remoteAddress: req.connection.remoteAddress
+			remoteAddress: ip
 		})
 	},
 
 
 
 	duplicateCheck: function() {
-		VARS.hash = `${VARS.game}-${VARS.player}-${VARS.turn}`.replace(/ /g, "-").toLowerCase()
+		VARS.hash = `${VARS.game}-${VARS.playerRaw}-${VARS.turn}`.replace(/ /g, "-").toLowerCase()
 
-		if (SENT_MESSAGES[VARS.hash] !== true) {
-			SENT_MESSAGES[VARS.hash] = true;
+		if (!SENT_MESSAGES[VARS.hash]) {
+			SENT_MESSAGES[VARS.hash] = VARS;
 		}
 		else {
 			VARS.errorMsg = `'${VARS.hash}' was already sent, skipping duplicate.`
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
+const fs = require('fs')
 app.use(bodyParser.json())
+app.engine('html', require('ejs').renderFile)
 
 // Discord helper
 DISCORD = require("webhook-discord")
@@ -14,9 +16,35 @@ VARS = {}
 
 // Port to bind the server to
 APP_PORT = 80
+APP_ALIVE_INTERVAL = 7200000
 
 // Object to store sent messages for duplicate checking
 SENT_MESSAGES = {}
+ERRORS = {}
+
+// Read old log.json entries from disk to memory
+fs.readFile('log.json', 'utf-8', (err, data) => {
+    if (err) throw err;
+	try {
+		SENT_MESSAGES = JSON.parse(data.toString())
+	}
+	catch(e) {
+		console.error("Could not parse log.json, starting fresh.")
+		SENT_MESSAGES = {}
+	}
+});
+
+// Read old errors.json entries from disk to memory
+fs.readFile('errors.json', 'utf-8', (err, data) => {
+    if (err) throw err;
+	try {
+		ERRORS = JSON.parse(data.toString())
+	}
+	catch(e) {
+		console.error("Could not parse log.json, starting fresh.")
+		ERRORS = {}
+	}
+});
 
 // toggle whether messages will actually be sent to discord, for debugging purposes
 SENDING_ENABLED = true
@@ -40,5 +68,15 @@ HOOKS = [
 	// },
 ]
 
+// Handle posts requests to /, /civ, /new_turn, /civ/new_turn
+app.post('/', CIV.handlePost)
+app.post('/civ', CIV.handlePost)
+app.post('/new_turn', CIV.handlePost)
 app.post('/civ/new_turn', CIV.handlePost)
+
+// Handle get requests to /, /alive
+app.get('/', CIV.showLog)
+app.get('/alive', CIV.handleAlive)
+
+// Launch
 CIV.startApp(app)
